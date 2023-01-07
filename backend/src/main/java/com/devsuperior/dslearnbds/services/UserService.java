6c4d926c3d054de179a4b3c97574a197c66f7c75
Diego R. Barbosa <diegokod@gmail.com>
2023-01-07 12:34:08 -0300

    Feat: Created exceptions methods to forbidden access

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -20,17 +20,22 @@ import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
 public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	
+	@Autowired
+	private UserRepository repository;
+
+	@Autowired
+	private AuthService authService;
 	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		authService.validatedSelfOrAdmin(id);
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
 	}
 	
-	@Autowired
-	private UserRepository repository;
-
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
